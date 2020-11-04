bbc238030af417383d164a8a4a554d2805ef3007
Dain Brownlow <dain.brownlow@outlook.com>
2020-11-04 02:18:39 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,10 +5,11 @@ def key_for_min_value(name_hash)
   if name_hash == {}
     return nil 
   else
-    keys = []
     values = []
     name_hash.each do |key, value|
-      
+      values.push(value)
     end
+    values = values.sort
+    
   end
 end
