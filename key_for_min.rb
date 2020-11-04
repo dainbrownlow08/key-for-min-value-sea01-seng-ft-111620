3a4c1aaf0c77315427aeea2b3ff45ebde7f3ee9a
Dain Brownlow <dain.brownlow@outlook.com>
2020-11-04 02:10:02 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  if name_hash == {}
+    return nil 
+  else
+    min_value = name_hash
+    name_hash.each do |name, value|
+      
+    
+      
+  end
 end
